ffb02a55316c19caf3bb856423054d2c633111bd
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2021-07-08 23:49:16 +0300

    Add code to index.php

--- src/index.php
+++ src/index.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Sample\MyFilter;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$file = __DIR__ . '/sample.xlsx';
+
+$reader = IOFactory::createReaderForFile($file);
+
+$reader->setReadFilter(new MyFilter());
+
+$spreadsheet = $reader->load($file);
+
+foreach ($spreadsheet->getAllSheets() as $sheet) {
+  var_dump($sheet->toArray(null, true, true, true));
+}
